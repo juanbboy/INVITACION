cf1ad9e7fa4a398f050b95661023a83e61b241b0
juanbboy <juanca.rr@hotmail.com>
2022-06-28 20:46:22 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Countdown from './components/countdown/Countdown';
+import Moda from './components/moda/Moda';
+import Moments from './components/moments/Moments';
+import Site from './components/sites/Site';
+import Start from './components/start/Start';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <div> */}
+    <Start />
+    <Countdown />
+    <Site />
+    <Moments />
+    <Moda />
+    {/* <footer className="pt-3 mt-4 text-muted border-top text-center"> */}
+    {/* &copy; 2022 */}
+    {/* </footer> */}
+    {/* </div> */}
   </React.StrictMode>
 );
 
