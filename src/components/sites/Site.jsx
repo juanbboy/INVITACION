397b5f27fcfd771332a85a286a94fb2dbb82398e
juanbboy <juanca.rr@hotmail.com>
2022-07-27 21:20:43 -0500

    make it better

--- src/components/sites/Site.jsx
+++ src/components/sites/Site.jsx
@@ -49,7 +49,7 @@ const Site = () => {
                         <img
                             className="w-100 jpg"
                             // src="../assets/012.png"
-                            srcSet="../assets/020.jpg w320, ../assets/012.jpg 1024w"
+                            srcSet="../assets/020.jpg 320w, ../assets/012.jpg 1024w"
                             alt="second slide"
                             height="525"
 
