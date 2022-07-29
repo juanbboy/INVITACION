1a82f53cbb524db95fb6abc60d367a42e5d014f2
juanbboy <juanca.rr@hotmail.com>
2022-07-28 21:27:43 -0500

    make it better

--- src/components/moments/Moments.jsx
+++ src/components/moments/Moments.jsx
@@ -9,32 +9,32 @@ const Moments = () => {
                 <img width="168" height="15" src="../assets/3.png" className="attachment-medium_large size-medium_large jetpack-lazy-image lazyloaded jetpack-lazy-image--handled" alt="" sizes="(max-width: 768px) 100vw, 768px" data-ll-status="loaded" data-lazy-loaded="1" loading="eager"></img>
             </div>
             <div className="row align-items-center p-4 ">
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/020.jpeg" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/020.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/021.jpeg" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/021.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/022.jpeg" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/022.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/023.jpg" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/023.jpg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/024.jpeg" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/024.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/025.jpeg" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/025.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/026.png" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/026.png" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/027.jpg" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/027.jpg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 img-fluid">
-                    <img src="../assets/028.png" width="100%" height="285" />
+                <div className="col-md-4 p-2">
+                    <img className='im' src="../assets/028.png" width="100%" />
                 </div>
             </div>
         </div>
