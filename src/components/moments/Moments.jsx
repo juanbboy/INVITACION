ebb9707379f2530f76612d86cbba1c2cde658061
juanbboy <juanca.rr@hotmail.com>
2022-07-28 22:19:38 -0500

    make it better

--- src/components/moments/Moments.jsx
+++ src/components/moments/Moments.jsx
@@ -9,31 +9,31 @@ const Moments = () => {
                 <img width="168" height="15" src="../assets/3.png" className="attachment-medium_large size-medium_large jetpack-lazy-image lazyloaded jetpack-lazy-image--handled" alt="" sizes="(max-width: 768px) 100vw, 768px" data-ll-status="loaded" data-lazy-loaded="1" loading="eager"></img>
             </div>
             <div className="row align-items-center p-4 ">
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/020.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/021.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/022.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/023.jpg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/024.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/025.jpeg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/026.png" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/027.jpg" width="100%" />
                 </div>
-                <div className="col-md-4 p-2 ">
+                <div className="col-sm-4 p-2 ">
                     <img className='mi' src="../assets/028.png" width="100%" />
                 </div>
             </div>
