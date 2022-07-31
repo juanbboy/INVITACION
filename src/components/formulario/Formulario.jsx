1dfc3ece05e882740509ccc9fc976203e071e2bf
juanbboy <juanca.rr@hotmail.com>
2022-07-31 14:18:35 -0500

    make it better

--- src/components/formulario/Formulario.jsx
+++ src/components/formulario/Formulario.jsx
@@ -3,7 +3,7 @@ import './formulario.css';
 
 const Formulario = () => {
     return (
-        <div className="conte text-center p-5" >
+        <div className="conte text-center p-5 f" >
             <div className="tex1 p-3" data-aos="fade-up">Confirma tu Asistencia</div>
             <div data-aos="fade-up">
                 <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=pmZCS2gTr0GtWlnrY0962NnveMndEHpBknonPKUckZBUMkdYT05DNEpJMDFWVVNBMThPTkNFMTJJRy4u" target="_blank" rel="noopener noreferrer">
