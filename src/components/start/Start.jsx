9d7099141d62e27cb14f199b0377ea4620a1af1a
juanbboy <juanca.rr@hotmail.com>
2022-07-31 09:40:40 -0500

    make it better

--- src/components/start/Start.jsx
+++ src/components/start/Start.jsx
@@ -20,21 +20,21 @@ const Start = () => {
             <h1 className='text2 p-4' data-aos="fade-up" style={{ color: "#3D4212", fontFamily: 'Cormorant Garamond', textAlign: "center", fontWeight: '450' }}>JUAN CARLOS <em>&amp;</em> ALEJANDRA</h1>
             <h1 className='text3' data-aos="fade-up" data-aos-delay="300" style={{ color: "#767B4B", fontFamily: 'Italianno', textAlign: "center", opacity: 0.8 }}>Acompañanos a Celebrar </h1>
             <div className="row justify-content-center">
-                <div className="col-md-4" >
+                <div className="col-md-2" >
                     <hr className='linea' />
                     <h1 data-aos="fade-up" data-aos-delay="400" style={{ color: "#3D4212", fontFamily: 'Cormorant Garamond', textAlign: "center", fontSize: '25px', fontWeight: 'bold' }}>DOMINGO</h1>
                     <hr className='linea' />
                 </div>
-                <div className="col-md-4 num" data-aos="fade-up" data-aos-delay="400">
+                <div className="col-md-2 num" data-aos="fade-up" data-aos-delay="400">
                     <div style={{ color: "#3D4212", fontFamily: 'Cormorant Garamond', textAlign: "center", letterSpacing: '5px' }}>16</div>
                 </div>
-                <div className="col-md-4 col1">
+                <div className="col-md-2 col1">
                     <hr className='linea' />
                     <div data-aos="fade-up" data-aos-delay="400" style={{ color: "#3D4212", fontFamily: 'Oxygen', textAlign: "center", letterSpacing: '6px' }}>2022</div>
                     <hr className='linea' />
                 </div>
                 <div className="row justify-content-center p-1" data-aos="fade-up" data-aos-delay="600">
-                    <div className="col-md-12 col2">
+                    <div className="col-md-2 col2">
                         <h1 style={{ color: "#3D4212", fontFamily: 'Italianno', textAlign: "center", fontSize: '45px' }}>Octubre</h1>
                     </div>
                 </div>
